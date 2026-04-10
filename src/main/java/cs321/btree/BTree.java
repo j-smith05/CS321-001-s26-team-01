a1bf426aeed9e93477ae44561e16320231e3046c
Jacob <jacobsmith214@u.boisestate.edu>
2026-04-10 17:20:51 -0600

    BTree.java almost complete

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -2,13 +2,15 @@ package cs321.btree;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BTree implements BTreeInterface {
 
-    private int degree;
-    private long size;              // number of UNIQUE keys in tree
+    private final int degree;
+    private long size;
     private long numberOfNodes;
-    private int height;             // root-only tree has height 0
+    private int height;
     private String fileName;
 
     private BTreeNode root;
@@ -101,7 +103,7 @@ public class BTree implements BTreeInterface {
         size++;
     }
 
-    private void insertNonFull(BTreeNode node, TreeObject obj) throws IOException {
+    private void insertNonFull(BTreeNode node, TreeObject obj) {
         int i = node.n - 1;
 
         if (node.leaf) {
@@ -129,33 +131,146 @@ public class BTree implements BTreeInterface {
         }
     }
 
+    private void splitChild(BTreeNode parent, int childIndex) {
+        BTreeNode fullChild = parent.children[childIndex];
+        BTreeNode newChild = new BTreeNode(fullChild.leaf);
 
-    private void splitChild(BTreeNode parent, int childIndex) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dumpToFile'");
-    }
+        // newChild gets t-1 keys
+        newChild.n = degree - 1;
 
-    @Override
-    public void dumpToFile(PrintWriter out) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dumpToFile'");
-    }
+        // copy last t-1 keys from fullChild to newChild
+        for (int j = 0; j < degree - 1; j++) {
+            newChild.keys[j] = fullChild.keys[j + degree];
+        }
 
-    @Override
-    public void dumpToDatabase(String dbName, String tableName) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dumpToDatabase'");
+        // if not leaf, copy last t children
+        if (!fullChild.leaf) {
+            for (int j = 0; j < degree; j++) {
+                newChild.children[j] = fullChild.children[j + degree];
+            }
+        }
+
+        // reduce fullChild key count
+        fullChild.n = degree - 1;
+
+        // shift parent's children right
+        for (int j = parent.n; j >= childIndex + 1; j--) {
+            parent.children[j + 1] = parent.children[j];
+        }
+        parent.children[childIndex + 1] = newChild;
+
+        // shift parent's keys right
+        for (int j = parent.n - 1; j >= childIndex; j--) {
+            parent.keys[j + 1] = parent.keys[j];
+        }
+
+        // move median key up to parent
+        parent.keys[childIndex] = fullChild.keys[degree - 1];
+        parent.n++;
+
+        numberOfNodes++;
     }
 
     @Override
     public TreeObject search(String key) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'search'");
+        return search(root, key);
+    }
+
+    private TreeObject search(BTreeNode node, String key) {
+        int i = 0;
+
+        while (i < node.n && key.compareTo(node.keys[i].getKey()) > 0) {
+            i++;
+        }
+
+        if (i < node.n && key.equals(node.keys[i].getKey())) {
+            return node.keys[i];
+        }
+
+        if (node.leaf) {
+            return null;
+        }
+
+        return search(node.children[i], key);
+    }
+
+    @Override
+    public void dumpToFile(PrintWriter out) throws IOException {
+        dumpToFile(root, out);
+    }
+
+    private void dumpToFile(BTreeNode node, PrintWriter out) {
+        for (int i = 0; i < node.n; i++) {
+            if (!node.leaf) {
+                dumpToFile(node.children[i], out);
+            }
+            out.println(node.keys[i].toString());
+        }
+
+        if (!node.leaf) {
+            dumpToFile(node.children[node.n], out);
+        }
+    }
+
+    @Override
+    public void dumpToDatabase(String dbName, String tableName) throws IOException {
+        // not implemented
     }
 
     @Override
     public void delete(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        // not implemented
+    }
+
+    public String[] getSortedKeyArray() {
+        List<String> keys = new ArrayList<>();
+        collectKeysInOrder(root, keys);
+        return keys.toArray(new String[0]);
+    }
+
+    private void collectKeysInOrder(BTreeNode node, List<String> keys) {
+        for (int i = 0; i < node.n; i++) {
+            if (!node.leaf) {
+                collectKeysInOrder(node.children[i], keys);
+            }
+            keys.add(node.keys[i].getKey());
+        }
+
+        if (!node.leaf) {
+            collectKeysInOrder(node.children[node.n], keys);
+        }
+    }
+
+    // ------------------------
+    // Disk Write / Read
+    // ------------------------
+
+
+
+    private void insertLoaded(TreeObject obj) {
+        if (root.n == 2 * degree - 1) {
+            BTreeNode newRoot = new BTreeNode(false);
+            newRoot.children[0] = root;
+            splitChild(newRoot, 0);
+            root = newRoot;
+            numberOfNodes++;
+            height++;
+        }
+
+        insertNonFull(root, obj);
+        size++;
+    }
+
+    private void collectObjectsInOrder(BTreeNode node, List<TreeObject> objects) {
+        for (int i = 0; i < node.n; i++) {
+            if (!node.leaf) {
+                collectObjectsInOrder(node.children[i], objects);
+            }
+            objects.add(node.keys[i]);
+        }
+
+        if (!node.leaf) {
+            collectObjectsInOrder(node.children[node.n], objects);
+        }
     }
 }
